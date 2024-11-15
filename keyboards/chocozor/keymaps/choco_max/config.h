e7022d07b689e5769bf313fd08e58dd41584a33e
Chukozor <9273715+Chukozor@users.noreply.github.com>
2024-11-16 00:52:56 +0100

    Update config.h

--- keyboards/chocozor/keymaps/choco_max/config.h
+++ keyboards/chocozor/keymaps/choco_max/config.h
@@ -72,7 +72,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // TRACKPAD
 #define SPLIT_POINTING_ENABLE
 #define POINTING_DEVICE_RIGHT
-#define CIRQUE_PINNACLE_CURVED_OVERLAY
+// #define CIRQUE_PINNACLE_CURVED_OVERLAY
 // #define DEBOUNCE 5
 // #define F_SCL 100000L
 
