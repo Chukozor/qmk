e792d8b6c318472e65d2e610c8ded5b8ef502301
Chukozor <9273715+Chukozor@users.noreply.github.com>
2025-02-20 23:59:09 +0100

    wip

--- keyboards/chocozor/keymaps/choco_max/keymap.c
+++ keyboards/chocozor/keymaps/choco_max/keymap.c
@@ -222,9 +222,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    XXXXXXX, XXXXXXX,      KC_F5, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2,
   //|--------+-----------+--------+-----------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    XXXXXXX,  KC_F10,LALT(KC_P1), XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LCTL,    XXXXXXX,   KC_P4,LALT(KC_P5),   KC_P6, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+-----------+--------+-----------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,LALT(KC_F2),   KC_F1,      KC_F2,   KC_F3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LSFT,LALT(KC_F2),   KC_P1,      KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------------------------------------------------------------|   |-------------------------------------------------------------|
                         XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX
                      //`-------------------------------------------'   `-------------------------------------------'
