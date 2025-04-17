94c87cc9afa57b81517cc3c6f81a1a8421a3d7e4
Chukozor <9273715+Chukozor@users.noreply.github.com>
2025-04-17 19:13:36 +0200

    raccourci pour toggle viseur

--- keyboards/chocozor/keymaps/choco_max/keymap.c
+++ keyboards/chocozor/keymaps/choco_max/keymap.c
@@ -217,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // _AUX_GAME for gaming
     [_AUX_GAME] = LAYOUT_split_3x6_4(
   //,-----------------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    XXXXXXX,    KC_H,      KC_F8, XXXXXXX,  KC_TAB,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2,
+       KC_ESC,LALT(KC_F7),    KC_H,      KC_F8, XXXXXXX,  KC_TAB,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2,
   //|--------+-----------+--------+-----------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    XXXXXXX,   KC_P4,LALT(KC_F5),   KC_P6, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+-----------+--------+-----------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
