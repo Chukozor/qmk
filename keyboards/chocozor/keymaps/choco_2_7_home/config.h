026bd53d0be36a83cdb9b4a4c3e951760f845682
Chukozor <9273715+Chukozor@users.noreply.github.com>
2025-09-14 16:05:28 +0200

    modifying in which config.h the FULL DUPLEX is written

--- keyboards/chocozor/keymaps/choco_2_7_home/config.h
+++ keyboards/chocozor/keymaps/choco_2_7_home/config.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
-#define SERIAL_USART_TX_PIN GP0     // USART TX pin
-#define SERIAL_USART_RX_PIN GP1     // USART RX pin
-
-
-
 //#define USE_MATRIX_I2C
 
 /* Select hand configuration */
