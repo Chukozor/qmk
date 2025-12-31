3a112c48d6f175935662ce7d2c4f5e4ceffacfe1
Chukozor <9273715+Chukozor@users.noreply.github.com>
2025-12-31 02:01:40 +0100

    added a RGB layer : it's compiling

--- keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
+++ keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------------------------------|    |-------------------------------------|
        KC_U, KC_V, FR_W, KC_X, KC_Y, FR_Z,     S(KC_U), S(KC_V), S(FR_W), S(KC_X), S(KC_Y), S(FR_Z), 
   //|-------------------------------------|    |-------------------------------------|
-      KC_P1,KC_P2,KC_P3,KC_P4,KC_P5,KC_P6,       KC_P7,   KC_P8,   KC_P9, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_P1,KC_P2,KC_P3,KC_P4,MO(_RGB),KC_P6,    KC_P7,   KC_P8,   KC_P9, KC_LEFT, KC_DOWN, KC_RIGHT
   //|-------------------------------------|    |-------------------------------------|
   ),
   // _COLEMAK_FR
@@ -49,6 +49,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------------------------------------------------------------|     |-------------------------------------------------------------|
       KC_LSFT,    FR_Q,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_K,    FR_M, FR_COMM,  FR_DOT, FR_QUES, KC_RSFT
   //|--------------------------------------------------------------|     |-------------------------------------------------------------|
+  ),
+  // _RGB
+    [_RGB] = LAYOUT_2_7_regular(
+  //--------------------------------------.    --------------------------------------.
+       FR_A, KC_B, KC_C, KC_D, KC_E, KC_F,     S(FR_A), S(KC_B), S(KC_C), S(KC_D), S(KC_E), S(KC_F), 
+  //|--------+--------+--------+--------+-|    |--------+--------+--------+--------+-|
+       KC_G, KC_H, KC_I, KC_J, KC_K, KC_L,     RGB_TOG, _I__MOD, _I_COUL, __I_LUM, __I_SAT, __I_VIT, 
+  //|--------+--------+--------+--------+-|    |--------+--------+--------+--------+-|
+       KC_M, KC_N, KC_O, KC_P, FR_Q, KC_R,     XXXXXXX, _D_RMOD, _D_COUL, __D_LUM, __D_SAT, __D_VIT,
+  //|-------------------------------------|    |-------------------------------------|
+       KC_U, KC_V, FR_W, KC_X, KC_Y, FR_Z,     S(KC_U), S(KC_V), S(FR_W), S(KC_X), S(KC_Y), S(FR_Z), 
+  //|-------------------------------------|    |-------------------------------------|
+      KC_P1,KC_P2,KC_P3,KC_P4,KC_P5,MO(_RGB),    KC_P7,   KC_P8,   KC_P9, KC_LEFT, KC_DOWN, KC_RIGHT
+  //|-------------------------------------|    |-------------------------------------|
   )
 };
 
@@ -60,4 +74,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
   [_DEBUGLAYER]   = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }, // Mapping for Base layer
   [_COLEMAK_FR]   = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }, // Mapping for Base layer
   [_REG_QWERTY]   = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }, // Mapping for Base layer
+  [_RGB]   = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }, // Mapping for Base layer
 };
--- keyboards/chocozor/keymaps/choco_2_7_home/keymap.h
+++ keyboards/chocozor/keymaps/choco_2_7_home/keymap.h
@@ -3,6 +3,18 @@
 enum layers{
     _DEBUGLAYER,
     _COLEMAK_FR,
-    _REG_QWERTY
+    _REG_QWERTY,
+    _RGB
 };
+
+#define _I__MOD RGB_MOD
+#define _D_RMOD RGB_RMOD
+#define __I_LUM RGB_VAI
+#define __D_LUM RGB_VAD
+#define _I_COUL RGB_HUI
+#define _D_COUL RGB_HUD
+#define __I_SAT RGB_SAI
+#define __D_SAT RGB_SAD
+#define __I_VIT RGB_SPI
+#define __D_VIT RGB_SPD
 
