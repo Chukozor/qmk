e12a88e0ef96f4f2f23ae4e97cadb1885a460d86
Michal <cizek.michal@gmail.com>
2026-01-17 23:17:17 +0100

    init l'oled

--- keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
+++ keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
@@ -81,7 +81,10 @@ static void solenoid_helper(void){
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
       solenoid_helper();
-      (void)oled_on();
+      if(!is_oled_on()){
+        (void)oled_init(0);
+        (void)oled_on();
+      }
       if(is_oled_on()){
         SEND_STRING("ABC");
         oled_write_P(PSTR("Hello, OLED!"), false);
