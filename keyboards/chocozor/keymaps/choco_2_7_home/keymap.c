101735850c5d0973b5e58599006c6a1482cc707e
Michal <cizek.michal@gmail.com>
2026-01-17 21:54:57 +0100

    Test oled minimal

--- keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
+++ keyboards/chocozor/keymaps/choco_2_7_home/keymap.c
@@ -2,7 +2,6 @@
 #include "keymap_french.h"
 #include "timer.h"
 #include "enum.h"
-#include "oled.h"
 #include "accents.h"
 #include "gaming.h"
 #include "globals.h"
@@ -485,7 +484,42 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
   [_RGB] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }
 };
 
+
+static void render_layer_status(void) {
+  //static const char* impression[]= {_LAYERS 0};
+  //oled_write(impression[get_highest_layer(layer_state)], false);
+  oled_write("TEEEST!",false); 
+  }
+
+static void render_logo(void) {
+  static const char PROGMEM logo[] = {
+    // 'cornia', 32x32px
+  0, 0, 0, 0, 0, 0, 0, 0, 192, 32, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 32, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 128, 192, 224, 255, 255, 240, 224, 200, 132, 132, 6, 6, 6, 6, 132, 132, 204, 224, 240, 255, 255, 224, 192, 192, 128, 0, 0, 0, 0, 12, 7, 3, 1, 0, 0, 28, 112, 192, 1, 1, 3, 3, 1, 224, 224, 17, 3, 3, 1, 1, 192, 112, 28, 0, 0, 1, 3, 7, 12, 0, 0, 0, 0, 0, 0, 0, 0, 64, 64, 64, 97, 98, 114, 56, 60, 31, 31, 60, 56, 114, 98, 97, 64, 64, 64, 0, 0, 0, 0, 0, 0, 0
+  };
+  oled_write_raw_P(logo, sizeof(logo));
+}
+
+bool oled_task_user() {
+  //oled_set_cursor(0, 0);
+  //render_logo();
+  //oled_set_cursor(0, 7);
+  render_layer_status();
+  return false;
+}
+
+void oled_render_boot(bool bootloader) {
+  oled_clear();
+  oled_set_cursor(0, 2);
+  if (bootloader) {
+    oled_write_P(PSTR("FLASH"), false);
+  } else {
+    oled_write_P(PSTR("RESET"), false);
+  }
+  oled_render_dirty(true);
+}
+
 bool shutdown_user(bool jump_to_bootloader) {
   oled_render_boot(jump_to_bootloader);
   return false;
 }
+
--- keyboards/chocozor/keymaps/choco_2_7_home/oled.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include QMK_KEYBOARD_H
-void oled_render_boot(bool);
