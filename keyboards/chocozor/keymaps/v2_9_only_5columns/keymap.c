5966ba7047677ed89fd3ea68b932b3d54f4ea20b
Chukozor <9273715+Chukozor@users.noreply.github.com>
2026-03-04 18:46:06 +0100

    cleaning keymap

--- keyboards/chocozor/keymaps/v2_9_only_5columns/keymap.c
+++ keyboards/chocozor/keymaps/v2_9_only_5columns/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM toggle_game[]              = {FR_A, KC_R, KC_S,KC_T, KC_D
 const uint16_t PROGMEM combo_clear_eeprom[]       = {RGB_TOG, _I__MOD, _I_COUL,__I_LUM, __I_SAT, COMBO_END};
 const uint16_t PROGMEM combo_web[]                = {MY_LCTL, MY_RCTL, COMBO_END};
 const uint16_t PROGMEM combo_print_screen[]       = {KC_R, KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_RGB[]                = {FR_Z,KC_1,KC_2,COMBO_END};
+const uint16_t PROGMEM combo_RGB[]                = {FR_Z,MY_LCTL,KC_LSFT,COMBO_END};
 
 // static bool solenoid_actif = false;
 
@@ -442,7 +442,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  XXXXXXX,    FR_W,    KC_F,    KC_P,    KC_G,                                                   KC_J,    KC_L,    KC_U,    KC_Y, FR_QUOT,
     FR_A,    KC_R,    KC_S,    KC_T,    KC_D,                                                   KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
     FR_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_K,    FR_M, FR_COMM,  FR_DOT, FR_QUES,
-             KC_O, MY_LCTL, KC_LSFT,                                                                  XXXXXXX, XXXXXXX,  KC_TAB,
+             KC_O, MY_LCTL, KC_LSFT,                                                                  KC_RSFT, MY_RCTL,  KC_TAB,
                                     MO_OX_G,  KC_I,  KC_SPC,                XXXXXXX,  KC_ENT, KC_LALT
 ),
 [_AUX_GAME] = LAYOUT_2_9_only_5_columns(
@@ -450,7 +450,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-           XXXXXXX, XXXXXXX, XXXXXXX,                                                                  XXXXXXX, XXXXXXX,  KC_TAB,
+           XXXXXXX, XXXXXXX, XXXXXXX,                                                                  KC_RSFT, MY_RCTL,  KC_TAB,
                                       XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX
 ),
 [_CAPS_LOCK] = LAYOUT_2_9_only_5_columns(
@@ -458,7 +458,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  XXXXXXX, S(FR_W), S(KC_F), S(KC_P), S(KC_G),                                                 S(KC_J), S(KC_L), S(KC_U), S(KC_Y), FR_QUOT,
  S(FR_A), S(KC_R), S(KC_S), S(KC_T), S(KC_D),                                                 S(KC_H), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
  S(FR_Q), S(KC_X), S(KC_C), S(KC_V), S(KC_B),                                                 S(KC_K), S(FR_M), FR_COMM,  FR_DOT, FR_QUES,
-          S(FR_Z), MY_LCTL, KC_LSFT,                                                                   XXXXXXX, XXXXXXX,  KC_TAB,
+          S(FR_Z), MY_LCTL, KC_LSFT,                                                                   KC_RSFT, MY_RCTL,  KC_TAB,
                                      KC_LGUI,  MY_NAV,  HT_SPC,            XXXXXXX, CSTM_ENT, KC_LALT
 ),
 [_F_KEYS] = LAYOUT_2_9_only_5_columns(
@@ -466,7 +466,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F16,   KC_F7,   KC_F8,   KC_F9,  KC_F12,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_F15,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_F14,   KC_F1,   KC_F2,   KC_F3,  KC_F10,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-           KC_F13, MY_LCTL, KC_LSFT,                                                                   XXXXXXX, XXXXXXX,  KC_TAB,
+           KC_F13, MY_LCTL, KC_LSFT,                                                                   KC_RSFT, MY_RCTL,  KC_TAB,
                                      XXXXXXX, KC_LCTL, XXXXXXX,             XXXXXXX, KC_LCTL, KC_LALT
 ),
 [_NUMPAD_RIGHT] = LAYOUT_2_9_only_5_columns(
@@ -474,7 +474,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 IMGLASS_CP,   KC_P7,   KY_P8,   KC_P9, KC_BSPC,                                               XXXXXXX,   KC_P7,   KC_P8,   KC_P9, XXXXXXX,
   MO_WEBBR,   KC_P4,   KY_P5,   KC_P6, KC_PDOT,                                               KC_PDOT,   KC_P4,   KC_P5,   KC_P6, XXXXXXX,
   MY_ALT_T,   KY_P1,   KY_P2,   KY_P3,  KC_ENT,                                                KC_ENT,   KC_P1,   KC_P2,   KC_P3,  KC_ENT,
-              KC_P0, MY_LCTL, KC_LSFT,                                                                 XXXXXXX, XXXXXXX,  KC_TAB,
+              KC_P0, MY_LCTL, KC_LSFT,                                                                 KC_RSFT, MY_RCTL,  KC_TAB,
                                        _______, _______, _______,            MO_OP, MO(_NAV), KC_LALT
  ),
 [_NAV] = LAYOUT_2_9_only_5_columns(
@@ -482,7 +482,7 @@ IMGLASS_CP,   KC_P7,   KY_P8,   KC_P9, KC_BSPC,                                 
 IMGLASS_CP,   KC_P7,   KY_P8,   KC_P9, KC_BSPC,                                               XXXXXXX, KC_PGUP,   KC_UP, KC_PGDN, XXXXXXX,
   MO_WEBBR,   KC_P4,   KY_P5,   KC_P6, KC_PDOT,                                               KC_BSPC, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_DEL,
   MY_ALT_T,   KY_P1,   KY_P2,   KY_P3,  KC_ENT,                                               XXXXXXX, KC_HOME, XXXXXXX,  KC_END, XXXXXXX,
-              KC_P0, MY_LCTL, KC_LSFT,                                                                 XXXXXXX, XXXXXXX,  KC_TAB,
+              KC_P0, MY_LCTL, KC_LSFT,                                                                 KC_RSFT, MY_RCTL,  KC_TAB,
                                        _______, _______, _______,            XXXXXXX, KC_LCTL, KC_LALT
 ),
 [_OPERATIONS] = LAYOUT_2_9_only_5_columns(
@@ -490,7 +490,7 @@ IMGLASS_CP,   KC_P7,   KY_P8,   KC_P9, KC_BSPC,                                 
  TG_NUMP, ____INF, ____SUP, XXXXXXX, XXXXXXX,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
  XXXXXXX, __MOINS, __EQUAL, ___PLUS, XXXXXXX,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
  XXXXXXX, _DIVISE, MY_PIPE, ___FOIS, XXXXXXX,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-          XXXXXXX, XXXXXXX, XXXXXXX,                                                                   XXXXXXX, XXXXXXX,  KC_TAB,
+          XXXXXXX, XXXXXXX, XXXXXXX,                                                                   KC_RSFT, MY_RCTL,  KC_TAB,
                                      XXXXXXX, NAV_F_OP, HT_SPC,              XXXXXXX, XXXXXXX, XXXXXXX
 ),
 [_WEB_BROWSER] = LAYOUT_2_9_only_5_columns(
@@ -498,7 +498,7 @@ IMGLASS_CP,   KC_P7,   KY_P8,   KC_P9, KC_BSPC,                                 
  XXXXXXX, TABTO_L, WEB_TAB, TABTO_R, PRIVATE,                                                 PRIVATE, TABTO_L, WEB_TAB, TABTO_R, XXXXXXX,
 OPENBOOK,   WEB_G, FF_T_ST,   WEB_D, FF_FENE,                                                 FF_FENE,   WEB_G, FF_T_ST,   WEB_D, XXXXXXX,
  XXXXXXX, WEB_DUP, THEBOOK, BOOKMAR, XXXXXXX,                                                 XXXXXXX, BOOKMAR, THEBOOK, XXXXXXX, XXXXXXX,
-          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   XXXXXXX, XXXXXXX,  KC_TAB,
+          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   KC_RSFT, MY_RCTL,  KC_TAB,
                                      KC_LGUI, MY_NAV,  HT_SPC,              KC_LALT, AUX_WEB, XXXXXXX
 ),
 [_MOUSE_LAYER] = LAYOUT_2_9_only_5_columns(
@@ -506,7 +506,7 @@ OPENBOOK,   WEB_G, FF_T_ST,   WEB_D, FF_FENE,                                   
  XXXXXXX, XXXXXXX, XXXXXXX, ACEL_ON,ACEL_OFF,                                                 KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
  K_BLITZ, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX,                                                 XXXXXXX, ZOOM_TR, XXXXXXX, XXXXXXX, XXXXXXX,
  K_SNIPE, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX,                                                 XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN2, XXXXXXX,
-          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   XXXXXXX, XXXXXXX, KC_BTN2,
+          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   KC_RSFT, MY_RCTL, KC_BTN2,
                                      KC_LSFT, KC_LCTL, MO(_ACCENTS),       KC_BTN1, KC_BTN3, TG_SCROL
 ),
 [_ACCENTS] = LAYOUT_2_9_only_5_columns(
@@ -514,7 +514,7 @@ OPENBOOK,   WEB_G, FF_T_ST,   WEB_D, FF_FENE,                                   
   XXXXXXX,  MY_UNDO, MY_REDO,  MY_CUT, MY_PRT_P,                                              XXXXXXX, XXXXXXX,    HT_U, XXXXXXX, XXXXXXX,
       HT_A, SEL_ALL, MY_SAVE, MY_COPY, MY_PRT_Z,                                               KC_BSPC, XXXXXXX, _______,    HT_I,MY_OCIRC,
  MY_COMENT, XXXXXXX,MY_CEDIL,MY_PASTE, MY_PRT_S,                                               XXXXXXX, XXXXXXX, FR_COLN, FR_SCLN, FR_EXLM,
-            XXXXXXX, MY_LCTL, KC_LSFT,                                                                 XXXXXXX, XXXXXXX,  KC_TAB,
+            XXXXXXX, MY_LCTL, KC_LSFT,                                                                  KC_RSFT, MY_RCTL,  KC_TAB,
                                        KC_LGUI, MY_NAV,  HT_SPC,           KC_LALT, CSTM_ENT, XXXXXXX
 ),
 [_REG_SPE] = LAYOUT_2_9_only_5_columns(
@@ -522,7 +522,7 @@ OPENBOOK,   WEB_G, FF_T_ST,   WEB_D, FF_FENE,                                   
 MY_TILD, FR_LBRC, XXXXXXX, FR_RBRC,MY_DIESE,                                                  FR_PERC,  FR_LCBR, FR_UNDS, FR_RCBR,    FR_AMPR,
   FR_AT, KC_PMNS,  KC_EQL, KC_PPLS, MY_DOLL,                                                  FR_ASTR,MY_BQUOT2, FR_QUOT, FR_DQUO, S(KC_MINS),
 XXXXXXX, KC_PSLS, MY_PIPE, KC_PAST, MY_EURO,                                                  ___CIRC,  FR_LPRN, _BACKSL, FR_RPRN,    XXXXXXX,
-         XXXXXXX, MY_LCTL, KC_LSFT,                                                                     XXXXXXX, XXXXXXX,  KC_TAB,
+         XXXXXXX, MY_LCTL, KC_LSFT,                                                                     KC_RSFT, MY_RCTL,  KC_TAB,
                                          _______, _______, _______,        _______, CSTM_ENT, _______
 ),
 [_MULTIMEDIA] = LAYOUT_2_9_only_5_columns(
@@ -530,7 +530,7 @@ XXXXXXX, KC_PSLS, MY_PIPE, KC_PAST, MY_EURO,                                    
  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                 XXXXXXX, XXXXXXX, KC_CALC, XXXXXXX, XXXXXXX,
  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                 KC_MSTP, KC_MPRV, KC_VOLD, KC_MNXT, KC_MUTE,
  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   XXXXXXX, XXXXXXX,  KC_TAB,
+          XXXXXXX, MY_LCTL, KC_LSFT,                                                                   KC_RSFT, MY_RCTL,  KC_TAB,
                                      XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, KC_MPLY, XXXXXXX
 ),
 [_RGB] = LAYOUT_2_9_only_5_columns(
