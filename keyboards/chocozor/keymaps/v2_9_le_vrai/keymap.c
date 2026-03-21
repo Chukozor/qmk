a8a80fefa7bb7a3d948563dd8e73b68542e908ff
Chukozor <9273715+Chukozor@users.noreply.github.com>
2026-03-21 01:31:31 +0100

    wip gaming layer

--- keyboards/chocozor/keymaps/v2_9_le_vrai/keymap.c
+++ keyboards/chocozor/keymaps/v2_9_le_vrai/keymap.c
@@ -463,11 +463,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      KC_LGUI,  MY_NAV,  HT_SPC,        MOFKEYS, CSTM_ENT, KC_LALT
 ),
 [_GAME] = LAYOUT_2_9_regular(
-  MY_ESC,  MY_ESC,  MY_ESC,   KC_P4,   KC_P5,   KC_P6,                                                  KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_P1,   KC_P2,
-  MY_ESC, XXXXXXX,    FR_W,    KC_F,    KC_P,    KC_G,                                                   KC_J,    KC_L,    KC_U,    KC_Y, FR_QUOT,  KC_TAB,
+  MY_ESC,  MY_ESC,   KC_P1,   KC_P2,   KC_P3,   KC_P4,                                                  KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_P1,   KC_P2,
+  MY_ESC,    FR_Q,    FR_W,    KC_F,    KC_P,    KC_G,                                                   KC_J,    KC_L,    KC_U,    KC_Y, FR_QUOT,  KC_TAB,
  MY_LCTL,    FR_A,    KC_R,    KC_S,    KC_T,    KC_D,                                                   KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_LCTL,
  KC_LSFT,    FR_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_K,    FR_M, FR_COMM,  FR_DOT, FR_QUES, KC_LSFT,
-                      KC_O, MY_LCTL, KC_LSFT,                                                                  KC_RSFT, MY_RCTL,  KC_TAB,
+                     KC_F1,   KC_F2,   KC_F3,                                                                  KC_RSFT, MY_RCTL,  KC_TAB,
                                               MO_OX_G,  KC_I,  KC_SPC,                XXXXXXX,  KC_ENT, KC_LALT
 ),
 [_AUX_GAME] = LAYOUT_2_9_regular(
