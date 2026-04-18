300147707ae8480a8e1c08d6abe6102398b118ab
Chukozor <9273715+Chukozor@users.noreply.github.com>
2026-04-18 21:48:12 +0200

    first try to make the oled work

---
+++ keyboards/chocozor/keymaps/michal_test/keymap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum layers{
+    _DEBUGLAYER,
+    _RGB
+};
+
+#define _I__MOD RGB_MOD
+#define _D_RMOD RGB_RMOD
+#define __I_LUM RGB_VAI
+#define __D_LUM RGB_VAD
+#define _I_COUL RGB_HUI
+#define _D_COUL RGB_HUD
+#define __I_SAT RGB_SAI
+#define __D_SAT RGB_SAD
+#define __I_VIT RGB_SPI
+#define __D_VIT RGB_SPD
+
